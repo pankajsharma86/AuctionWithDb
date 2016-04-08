c69c84e038f9f19884bc0107658a81ba0eed8fb5
Pankaj Sharma <p.sharma511@yahoo.co.nz>
2016-04-08 14:46:12 +1200

    Auction Form complete (HttpPost)

--- AuctionWithDb/Controllers/AuctionController.cs
+++ AuctionWithDb/Controllers/AuctionController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AuctionWithDb.Models;
+using System.Web.Security;
 
 namespace AuctionWithDb.Controllers
 {
@@ -20,8 +22,31 @@ namespace AuctionWithDb.Controllers
             return View();
         }
 
-       
+        [HttpPost]
+        public ActionResult Auction([Bind(Exclude = "CurrentPrice")]Auction model)
+        {
+            using (var db = new AuctionEntities())
+            {
+                var bid = new Auction
+                {
+                    Category = model.Category,
+                    Title = model.Title,
+                    Description = model.Description,
+                    ImageUrl = model.ImageUrl,
+                    StartTime = model.StartTime,
+                    Endtime = model.Endtime,
+                    StartPrice = model.StartPrice,
+                    CurrentPrice = model.CurrentPrice,
+                };
 
-       
+                db.Auctions.Add(bid);
+                db.SaveChanges();
+            }
+                return View();
+        }
+
+
+
+
     }
 }
